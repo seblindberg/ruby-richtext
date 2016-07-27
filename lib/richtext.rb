dc287af0ba654ad4bc9a3d08fb1dc9a3af61ce69
Sebastian Lindberg <seb.lindberg@gmail.com>
2016-07-27 20:57:57 +0200

    Updated commenting style

--- lib/richtext.rb
+++ lib/richtext.rb
@@ -8,10 +8,9 @@ require 'richtext/document'
 module RichText
 end
 
-# RichText
-#
 # Convenience method for creating RichText objects. Calling RichText(obj) is
 # equivalent to RichText::Document.new(obj).
+
 def RichText(string)
   RichText::Document.new string
 end
